1ba01c4caf0138cea24152fd319186c81948ce3e
helowken <helowken@126.com>
2020-04-25 03:31:48 +0800

    sync cost_time command name

--- client/src/main/java/agent/client/command/parser/impl/CostTimeCmdParser.java
+++ client/src/main/java/agent/client/command/parser/impl/CostTimeCmdParser.java
@@ -8,6 +8,6 @@ public class CostTimeCmdParser extends AbstractTransformCmdParser {
 
     @Override
     public String getCmdName() {
-        return "costTime";
+        return "cost_time";
     }
 }
