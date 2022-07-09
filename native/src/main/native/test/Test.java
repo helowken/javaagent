5a90b0d9c3eac0fd6ae1b50d8cfed6411ac1b2dc
helowken <helowken@126.com>
2022-07-10 01:53:48 +0800

    remove unused files

--- native/src/main/native/test/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) throws Exception {
-        while (true) {
-            System.out.println("1111");
-            Thread.sleep(5000);
-        }
-    }
-}
--- native/src/main/native/util.h
+++
@@ -1,9 +0,0 @@
-#include <fcntl.h>
-
-void outputError(const char*, ...);
-
-int changeCredential(pid_t pid);
-
-int resetCredential();
-
-int attachJvm(int pid, const char *jarPath, const char *agentArgs);
